fd7bfb3de90a4f9e627f3d5432dfb2267252c5c3
Felipe Ramirez <73697209+Mc-Ramirez@users.noreply.github.com>
2021-12-15 13:38:05 +0100

    Initial Commit

---
+++ Liga Santander/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Liga Santander
+//
+//  Created by Felipe Ramírez on 15/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
